dfe12635181d04c30d8374222e524bbc9816d357
Shalini <shaliniyadav1801@gmail.com>
2021-06-21 03:04:03 +0530

    Additional Form Details

--- frontend/src/podcast/CreateEditPodcast.js
+++ frontend/src/podcast/CreateEditPodcast.js
@@ -1,5 +1,5 @@
 import { LeftOutlined } from "@ant-design/icons";
-import { Form, Modal, Button, Input, Select, Checkbox } from "antd";
+import { Form, Modal, Button, Input, Select, Checkbox, Radio} from "antd";
 import React, { useState } from "react";
 
 const { Option } = Select;
@@ -11,13 +11,20 @@ const CreateEditPodcast = () => {
   const [domain, setDomain] = useState(null);
   const [lang, setLang] = useState("en");
   const [isExplicit, setIsExplicit] = useState(null);
+  const [aname, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [copyright, setCopyright] = useState(null);
+  const [category, setCategory] = useState("tc");
+  const [radio, setRadio] = useState(1);
+  const [pcategory, setpCategory] = useState("tc");
+  
 
   const showModal = () => {
     setIsModalVisible(true);
   };
 
   const handleOk = () => {
-    console.log(title, description, domain, lang, isExplicit);
+    console.log(title, description, domain, lang, isExplicit, aname, email, copyright);
     // http request to server
     setIsModalVisible(false);
   };
@@ -36,7 +43,12 @@ const CreateEditPodcast = () => {
   const onFinish = (values) => {
     console.log(values);
   };
-  return (
+
+  const onChange = e => {
+    console.log('radio checked', e.target.value);
+    setRadio(e.target.value);
+  };
+   return (
     <>
       <Button type="primary" onClick={showModal}>
         Create a new podcast
@@ -100,6 +112,63 @@ const CreateEditPodcast = () => {
           >
             This episode includes explicit content.{" "}
           </Checkbox>
+          <h3>Author Details</h3>
+          <Form.Item label="Name">
+            <Input
+              placeholder="Your name"
+              value={aname}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item label="Email">
+            <Input
+              placeholder="abc123@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item label="Copyright">
+            <Input
+             // placeholder="Your name"
+              value={copyright}
+              onChange={(e) => setCopyright(e.target.value)}
+            />
+          </Form.Item>
+          <h3>Categories</h3>
+          <Form.Item label="Category">
+            <Input.Group compact>
+              <Form.Item name={"category"} noStyle>
+                <Select
+                  placeholder="Category"
+                  value={category}
+                  onChange={(val) => setCategory(val)}
+                >
+                  <Option value="tc">Tech</Option>
+                  <Option value="nc">Non-Tech</Option>
+                </Select>
+              </Form.Item>
+            </Input.Group>
+          </Form.Item>
+          <Form.Item label="Active Categories">
+          <Radio.Group onChange={onChange} value={radio} buttonStyle="solid">
+          <Radio value={1}>Tech</Radio>
+          <Radio value={2}>NonTech</Radio>
+          </Radio.Group>
+          </Form.Item>
+          <Form.Item label="Primary Category">
+            <Input.Group compact>
+              <Form.Item name={"pcategory"} noStyle>
+                <Select
+                  placeholder="Tech"
+                  value={pcategory}
+                  onChange={(val) => setpCategory(val)}
+                >
+                  <Option value="tc">Tech</Option>
+                  <Option value="nc">Non-Tech</Option>
+                </Select>
+              </Form.Item>
+            </Input.Group>
+            </Form.Item>
         </Form>
       </Modal>
     </>
