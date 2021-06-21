ac1d5e5c618c1ad2eeb5c87cc239a95cade02a8f
h4rdw1r3 <vinaugust6@gmail.com>
2021-06-21 11:58:26 +0530

    create a podcast list component (incomplete)

--- frontend/src/home/HomePage.js
+++ frontend/src/home/HomePage.js
@@ -1,13 +1,14 @@
 import Layout from "antd/lib/layout/layout";
 import React from "react";
 import NavBar from "../nav/NavBar";
-import NoPodcast from "./NoPodcast";
+// import NoPodcast from "./NoPodcast";
+import PodcastList from "../podcast/PodcastList";
 
 const HomePage = () => {
   return (
     <Layout>
       <NavBar />
-      <NoPodcast />
+      <PodcastList />
     </Layout>
   );
 };
